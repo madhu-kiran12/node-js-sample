88988b174f461c680a34d1faa19872754964140d
Ubuntu <ubuntu@ip-172-31-17-222.ec2.internal>
2025-12-17 11:55:33 +0000

    added somthing in index

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!, testing webhooks to auto trigger jenkins pipeline')
+  response.send('Hello World!, testing webhooks to auto trigger and run jenkins pipeline')
 })
 
 app.listen(app.get('port'), '0.0.0.0', function() {
